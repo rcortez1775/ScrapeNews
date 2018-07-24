38d7b7ca1ae672d27dba1db3bfc30d8e6af1d5e5
Robert Cortez <Nuhuhfoo@gmail.com>
2018-07-24 08:23:22 -0700

    assignment

---
+++ routes/view/index.js
@@ -0,0 +1,13 @@
+var router = require("express").Router();
+
+
+router.get("/", function(req, res) {
+  res.render("home");
+});
+
+
+router.get("/saved", function(req, res) {
+  res.render("saved");
+});
+
+module.exports = router;
